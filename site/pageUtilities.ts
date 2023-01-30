1d994f023345756511d66c171bc43c7bea5bf74f
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-30 15:46:11 +0200

    chore: Update gustwind

--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,8 +1,13 @@
 import md from "./transforms/markdown.ts";
-import type { Context } from "https://deno.land/x/gustwind@v0.30.1/breezewind/types.ts ";
+import type { Context } from "https://deno.land/x/gustwind@v0.39.4/breezewind/types.ts";
+import type { Routes } from "https://deno.land/x/gustwind@v0.39.4/types.ts";
 
-function markdown(_: Context, input: string) {
-  return md(input).content;
+function init({ routes }: { routes: Routes }) {
+  function markdown(_: Context, input: string) {
+    return md(input).content;
+  }
+
+  return { markdown };
 }
 
-export { markdown };
+export { init };
