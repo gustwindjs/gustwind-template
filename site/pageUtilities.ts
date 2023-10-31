6604359c6bce2ab4e5698fe2205b7dea64043bcc
Juho Vepsäläinen <bebraw@gmail.com>
2023-10-31 17:14:49 +0200

    chore: Update to 0.41.4

--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,9 +1,8 @@
 import md from "./transforms/markdown.ts";
-import type { Context } from "https://deno.land/x/gustwind@v0.39.4/breezewind/types.ts";
 import type { Routes } from "https://deno.land/x/gustwind@v0.39.4/types.ts";
 
 function init({ routes }: { routes: Routes }) {
-  function markdown(_: Context, input: string) {
+  function markdown(input: string) {
     return md(input).content;
   }
 
