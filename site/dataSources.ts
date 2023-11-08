6a5506ad02cda02d66a5547f2d12fbc4fbc103ef
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-08 10:49:31 +0200

    chore: Update gustwind

--- site/dataSources.ts
+++ site/dataSources.ts
@@ -1,12 +1,22 @@
+import type { LoadApi } from "https://deno.land/x/gustwind@v0.52.3/types.ts";
 import markdown from "./transforms/markdown.ts";
 
-async function processMarkdown(filename: string) {
-  // Drop title from the first line
-  // This is not cleanest solution since sometimes you have something else there!
-  // TODO: It would be better to check for the existence of # before removing the line
-  return markdown(
-    (await Deno.readTextFile(filename)).split("\n").slice(1).join("\n"),
-  );
+function init({ load }: LoadApi) {
+  async function processMarkdown(
+    filename: string,
+    o?: { skipFirstLine: boolean },
+  ) {
+    // Drop title from the first line
+    // This is not cleanest solution since sometimes you have something else there!
+    // TODO: It would be better to check for the existence of # before removing the line
+    const lines = await load.textFile(filename);
+
+    return markdown(
+      o?.skipFirstLine ? lines.split("\n").slice(1).join("\n") : lines,
+    );
+  }
+
+  return { processMarkdown };
 }
 
-export { processMarkdown };
+export { init };
--- site/globalUtilities.ts
+++ site/globalUtilities.ts
@@ -1,4 +1,4 @@
-import type { Routes } from "https://deno.land/x/gustwind@v0.39.4/types.ts";
+import type { Routes } from "https://deno.land/x/gustwind@v0.52.3/types.ts";
 
 function init({ routes }: { routes: Routes }) {
   // Add your global page utilities here.
